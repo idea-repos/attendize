6934386b072ef6846abf850d81e809e6b631cb5f
aman <er.amandeep06@gmail.com>
2022-05-03 15:44:50 +0530

    adding Razorpay

--- app/Services/PaymentGateway/Factory.php
+++ app/Services/PaymentGateway/Factory.php
@@ -70,7 +70,7 @@ class Factory
                     // $gateway = Omnipay::create($name);
                     // $gateway->initialize($paymentGatewayConfig);
 
-                    return new \Services\PaymentGateway\Razorpay();
+                    return new Razorpay();
                 }
         }
     }
