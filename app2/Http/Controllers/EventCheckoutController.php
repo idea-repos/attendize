ef9b121827272e1496c2994179231aba85731470
aman <er.amandeep06@gmail.com>
2022-05-03 14:47:24 +0530

    adding Razorpay

--- app2/Http/Controllers/EventCheckoutController.php
+++ app2/Http/Controllers/EventCheckoutController.php
@@ -335,13 +335,22 @@ class EventCheckoutController extends Controller
                 'messages' => $order->errors(),
             ]);
         }
-
+        $api_key = "rzp_test_T9UtxZ8dbDcv3K";
+        $api_secret = "F8saAdKU1R3YbSnOHcYQS2fb";
+        $api = new Api($api_key, $api_secret);
+        // dump($api);
+        $link = $api->paymentLink->create(array('amount'=>500, 'currency'=>'INR', 'accept_partial'=>true,
+        'first_min_partial_amount'=>100, 'description' => 'For XYZ purpose', 'customer' => array('name'=>'Gaurav Kumar',
+        'email' => 'gaurav.kumar@example.com', 'contact'=>'+919999999999'),  'notify'=>array('sms'=>true, 'email'=>true) ,
+        'reminder_enable'=>true ,'notes'=>array('policy_name'=> 'Jeevan Bima'),'callback_url' => route('razorCallback'),
+        'callback_method'=>'get'));
         return response()->json([
             'status'      => 'success',
-            'redirectUrl' => route('showEventPayment', [
-                    'event_id'    => $event_id,
-                    'is_embedded' => $this->is_embedded
-                ])
+            'redirectUrl' => $link
+            // 'redirectUrl' => route('showEventPayment', [
+            //         'event_id'    => $event_id,
+            //         'is_embedded' => $this->is_embedded
+            //     ])
         ]);
 
     }
