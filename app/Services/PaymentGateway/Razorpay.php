e828741e10b8fdaabd757eaa6b194eb05edf3685
aman <er.amandeep06@gmail.com>
2022-05-03 15:59:42 +0530

    adding Razorpay

--- app/Services/PaymentGateway/Razorpay.php
+++ app/Services/PaymentGateway/Razorpay.php
@@ -58,7 +58,7 @@ class Razorpay
     public function extractRequestParameters($request)
     {
         dump($request->all());
-        dump($this);
+        dump($this->extra_params);
         foreach ($this->extra_params as $param) {
             if (!empty($request->get($param))) {
                 $this->options[$param] = $request->get($param);
