21f17ed2ef6362a3ece94e925d64f640899a8709
aman <er.amandeep06@gmail.com>
2022-05-05 19:05:16 +0530

    added attendees

--- resources/views/Public/ViewEvent/EventPageViewOrder.blade.php
+++ resources/views/Public/ViewEvent/EventPageViewOrder.blade.php
@@ -5,10 +5,19 @@
 @stop
 
 @section('content')
+<section class="home-banner attendize">
+        <header>
+            <div class="logo"><a href="javascript:;"><img src="{{ url('/') }}/images/logo-black.svg" alt="logo" /></a></div>
+            <div class="trigger trigger-black"><a href="javascript:;">
+                    <span></span>
+                    <span></span>
+                </a></div>
+        </header>
+</section>
+<div class="event-spacer"></div>
 
-    @include('Public.ViewEvent.Partials.EventHeaderSection')
     <!-- @include('Public.ViewEvent.Partials.EventShareSection') -->
     @include('Public.ViewEvent.Partials.EventViewOrderSection')
-    @include('Public.ViewEvent.Partials.EventFooterSection')
+    <!-- @include('Public.ViewEvent.Partials.EventFooterSection') -->
     
 @stop
