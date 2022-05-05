9cf27002163638124e244e49328c93b5fcc60189
aman <er.amandeep06@gmail.com>
2022-05-05 18:35:03 +0530

    added attendees

--- resources/views/Public/ViewEvent/EventPageViewOrder.blade.php
+++ resources/views/Public/ViewEvent/EventPageViewOrder.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
     @include('Public.ViewEvent.Partials.EventHeaderSection')
-    @include('Public.ViewEvent.Partials.EventShareSection')
+    <!-- @include('Public.ViewEvent.Partials.EventShareSection') -->
     @include('Public.ViewEvent.Partials.EventViewOrderSection')
     @include('Public.ViewEvent.Partials.EventFooterSection')
     
