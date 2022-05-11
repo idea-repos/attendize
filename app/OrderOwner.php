0d3e3c45e75caeabbf51e67e9dc5766595ef96b6
aman <er.amandeep06@gmail.com>
2022-05-11 19:23:14 +0530

    adding login functionality

--- app/OrderOwner.php
+++ app/OrderOwner.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App;
-
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class OrderOwner extends Model
+class OrderOwner extends Authenticatable
 {
     //
 }
