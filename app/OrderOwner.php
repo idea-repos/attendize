2f7355d802130c692573eac23b599e44e656bf80
aman <er.amandeep06@gmail.com>
2022-05-11 23:40:58 +0530

    Adding user

--- app/OrderOwner.php
+++ app/OrderOwner.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Order;
 use CreateOrderOwnerOrdersTable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,19 @@ use Illuminate\Database\Eloquent\Model;
 class OrderOwner extends Authenticatable
 {
     //
-    public function orders(){
-        return $this->hasMany(OrderOwnerOrders::class);
+    // public function orders(){
+    //     return $this->hasMany(OrderOwnerOrders::class);
+    // }
+
+    public function orders()
+    {
+        return $this->hasManyThrough(
+            Order::class,
+            OrderOwnerOrders::class,
+            'order_owner_orders_id', // Foreign key on users table...
+            'order_id', // Foreign key on posts table...
+            'id', // Local key on countries table...
+            'id' // Local key on users table...
+        );
     }
 }
