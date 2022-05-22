f04c5e8a048dfebdc28a315f331d65b85b9a2faa
aman <er.amandeep06@gmail.com>
2022-05-22 16:27:17 +0530

    added home page functionality

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -17,12 +17,7 @@
 <div class="community-outer">
     <div class="wrapper">
         <div class="community-inner community-inner1">
-            <div class="community-left">
-                <img src="images/connect1-banner.png">
-                <div class="scroll-down">
-                   <span> SCROLL DOWN</span>
-                </div>
-            </div>
+            
             <div class="community-right">
                 <h1>Hey<span>{{auth('attendee')->user()->first_name}}</span> Welcome!</h1>
                 <p>Here you can find the Event Details</p>
