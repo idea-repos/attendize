b087fc1bf75169f6dc4347270c62caed0ff3d46c
aman <er.amandeep06@gmail.com>
2022-05-28 21:35:55 +0530

    added original Price

--- resources/views/Public/ViewEvent/Partials/EventTicketsSection.blade.php
+++ resources/views/Public/ViewEvent/Partials/EventTicketsSection.blade.php
@@ -44,8 +44,8 @@
                                                     $is_free_event = false;
                                                     ?>
                                                     <span title='{{money($ticket->price, $event->currency)}} @lang("Public_ViewEvent.ticket_price") + {{money($ticket->total_booking_fee, $event->currency)}} @lang("Public_ViewEvent.booking_fees")'>{{money($ticket->total_price, $event->currency)}} </span>
+                                                    <s>{{money($ticket->original_price)}}</s>
                                                     <span class="tax-amount text-muted text-smaller">
-                                                        @dump($ticket->original_price)
                                                         
                                                         {{ ($event->organiser->tax_name && $event->organiser->tax_value) ? '(+'.money(($ticket->total_price*($event->organiser->tax_value)/100), $event->currency).' '.$event->organiser->tax_name.')' : '' }}</span>
                                                     <meta property="priceCurrency"
