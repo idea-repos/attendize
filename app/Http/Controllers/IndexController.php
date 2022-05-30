cb04731bf412a216f972385ba40e3d28deb414ec
aman <er.amandeep06@gmail.com>
2022-05-30 19:06:45 +0530

    filter event s

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -66,7 +66,7 @@ class IndexController extends Controller
             $myevent['id'] = $value->id;
             $listEvent[] = $myevent;
         }
-        dd(json_encode($listEvent));
+        //dd(json_encode($listEvent));
         $data = [
             'events'    => $events,
             'listEvent' =>json_encode($listEvent),
