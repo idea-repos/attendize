ce708ee435d976634b578f0bde5e1b5caa48e0c6
Ksingh531@myseneca.ca <Ksingh531@myseneca.ca>
2022-06-04 16:12:14 +0530

    sendmail

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -15,7 +15,12 @@ body{
                 </a></div>
         </header>
 </section>
-
+@if (Session::has('success'))
+<div class="alert alert-success">{{Session::get('success')}}</div>
+@endif
+@if (Session::has('error'))
+<div class="alert alert-danger">{{Session::get('error')}}</div>
+@endif
 <div class="event-spacer"></div>
 
 
